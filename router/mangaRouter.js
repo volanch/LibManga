5ea0860bd7ae4366754d179267cec01b60fcb0ec
Diyarka <sikizhanov92@BK.RU>
2026-02-02 14:05:35 +0500

    v2.0 remade to node.js

---
+++ router/mangaRouter.js
@@ -0,0 +1,13 @@
+
+const path = require("path");
+const express = require("express");
+const router = express.Router();
+
+
+
+
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../beginning", "index.html"));
+})
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const cors = require('cors');
+const app = express();
+app.use(cors());
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.error("MongoDB error:", err));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+
+const mangaRouter = require("./router/mangaRouter");
+app.use("/", mangaRouter);
+
+app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
